a3be67cba4e9ad3ba819943f3db20558302b4560
zhangyezi@kaitaiming.com <zhangyezi@kaitaiming.com>
2020-10-19 20:36:23 +0800

    增加查询方式

--- myStudy/Server/server.js
+++ myStudy/Server/server.js
@@ -1,10 +1,3 @@
-/*// 导入http模块
-var http = require('http');
-http.createServer(function (req, res) {
-  res.write('<head><meta charset="utf-8"/></head>');
-  res.write('成功');
-  res.end('Hello World');
-}).listen(8000);*/
 var bodyParser = require('body-parser');
 //对数据进行解析
 var urlencodeParser = bodyParser.urlencoded({extended:true});
@@ -85,3 +78,54 @@ const port = 9000;
 app.listen(port, hostname, () => {
   console.log(`❗❗❗ 服务器开启在 http://${hostname}:${port}/`)
 })
+/*
+//获取数据
+app.get('/todo',function (req,res) {
+  // res.render('todo',{todos:data});
+  let sql = 'SELECT * FROM todo_text  WHERE `check`=0; SELECT * FROM todo_text  WHERE `check`=1';
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    // console.log(result)
+    res.render('todo',{todos:result[0],checks:result[1]})
+  })
+});
+//传递数据
+app.post('/todo',urlencodeParser,function (req,res) {
+  // data.push(req.body);
+  let text = req.body;
+  console.log(text);
+  let sql = "INSERT INTO todo_text SET ?";
+  db.query(sql, text, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+  })
+});
+// 删除数据
+app.delete('/todo/:id',function (req,res) {
+  // console.log(req.params.item);
+  // Todo.find({item:req.params.item}).remove(function (err,data) {
+  //     if (err) throw err;
+  //     res.json(data);
+  // })
+  // data = data.filter(function (todo) {
+  //     return req.params.item !== todo.item;
+  // });
+  let sql = `DELETE FROM todo_text WHERE ID = ${req.params.id}`;
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.json(result);
+  })
+
+  // res.json(data);
+});
+//更新数据
+app.get('/updatetodo/:id',(req, res) => {
+  let sql = "UPDATE `todo_text` SET `check` =1-`check` WHERE id = " +`${req.params.id}`;
+  console.log(sql);
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.json(result);
+  });
+});*/
